5991f7d1f3e6f38bf3fb307869a58efefab94ff5
SethSenpai <pimwing@gmail.com>
2018-01-23 15:30:55 +0000

    added crash command for softer reboots than closing the screen demon

--- miraBot.js
+++ miraBot.js
@@ -24,7 +24,6 @@ const loginObj = require('./data/login.json');
 
 //GLOBAL VARIABLES
 const client = new Discord.Client();
-var rl = readline.createInterface(process.stdin, process.stdout);
 var helpTextString;
 var creatorID;
 
@@ -48,6 +47,11 @@ client.on('message', message => {
 			console.log(funcFile.getDateTime() + " Help requested ".cyan + "Name: ".cyan + message.author.username);
 		}
 
+		if(message.content === "!emp" && message.author.id == loginObj.creatorID){
+			console.log(funcFile.getDateTime() + " Crashing now! ".red);
+			let elegance = crashing[666];
+		}
+
   });
   
 client.on("guildCreate", guild => {
@@ -67,7 +71,7 @@ helpTextString = "I have several functions that you can use: \n " +
 "[!8ball] ask me a question. \n "+
 "[!pet] or [!pat] to pet me for being a good robot! \n "+
 "[!roll xdy] to have me roll some dice, example: !roll 2d6 \n "+
-"[!hentai tag] lets me post a random lewd picture if there is not tag specified, otherwise its a lewd picture with the tag specified. \n"+
+"[!hentai tag] lets me post a random lewd picture if there is not tag specified, otherwise its a lewd picture with the tags specified. Different tags should be separated by a space \n"+
 "[!moe tag] works the same way as its lewder counterpart but less lewd. (generally sfw) \n"+
 "There are also some private memes hidden in my programming, maybe you will find them, maybe not. \n"+
 "You can ask me to join a server by clicking this link: https://discordapp.com/oauth2/authorize?client_id=389178140435546112&scope=bot \n" +
